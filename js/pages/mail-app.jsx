a794acfa73d1d5945f9e9d491cbd688d8a754624
Shon Shinkarenko <shon2789@gmail.com>
2021-08-25 13:05:01 +0300

    Added mail layout

--- js/pages/mail-app.jsx
+++ js/pages/mail-app.jsx
@@ -1,8 +1,21 @@
+import { MailInboxSvg } from "../apps/mail/cmps/mail-inbox-svg.jsx";
+import { MailIndex } from "../apps/mail/pages/mail-index.jsx";
+
+
 export class MailApp extends React.Component {
 
     render() {
         return (
-            <section>Mail</section>
+            <section className="">
+                <div className=" mail-side-nav">
+                    <div className="side-nav side-nav-inbox"><div className="side-nav-icon"><i className="fas fa-inbox"></i></div> Inbox</div>
+                    <div className="side-nav"> <div className="side-nav-icon"><i className="fas fa-star"></i></div> Starred</div>
+                    <div className="side-nav"><div className="side-nav-icon"><i className="fas fa-paper-plane"></i></div> Sent</div>
+                    <div className="side-nav"><div className="side-nav-icon"><i className="fas fa-file"></i></div> Drafts</div>
+                    <div className="side-nav"><div className="side-nav-icon"><i className="fas fa-trash"></i></div> Trash</div>
+                </div>
+                {/* <MailIndex /> */}
+            </section>
         )
     }
 }
