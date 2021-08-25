a4eb592072d8347f183ff0a44b8a9d8e357528f2
Jonathan Geiger <tantangeiger@gmail.com>
2021-08-25 11:46:04 +0300

    test

--- js/root-cmp.jsx
+++ js/root-cmp.jsx
@@ -1,26 +1,20 @@
-import { Footer } from "./cmps/footer/footer.jsx"
-import { Header } from "./cmps/header/header.jsx"
-import { AppHome } from "./pages/app-home.jsx"
+import { Footer } from './cmps/footer/footer.jsx';
+import { Header } from './cmps/header/header.jsx';
+import { AppHome } from './pages/app-home.jsx';
 
-
-const Router = ReactRouterDOM.HashRouter
-const { Route, Switch } = ReactRouterDOM
-
-
+const Router = ReactRouterDOM.HashRouter;
+const { Route, Switch } = ReactRouterDOM;
 
 export function App() {
-    return (
-        <Router>
-            <Header />
-            <section className="app">
-                <Switch>
-                    <Route path="/" component={AppHome} />
-                </Switch>
-                <Footer />
-            </section>
-        </Router>
-    )
+  return (
+    <Router>
+      <Header />
+      <section className="app">
+        <Switch>
+          <Route path="/" component={AppHome} />
+        </Switch>
+        <Footer />
+      </section>
+    </Router>
+  );
 }
-
-
-
