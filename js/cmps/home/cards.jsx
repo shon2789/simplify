470a15eec4eb023b619546000f5eb6616b75d21d
Jonathan Geiger <tantangeiger@gmail.com>
2021-08-27 22:38:20 +0300

    changed card text

--- js/cmps/home/cards.jsx
+++ js/cmps/home/cards.jsx
@@ -2,35 +2,40 @@ import { BookSvg } from "../book-svg.jsx"
 import { KeepSvg } from "../keep-svg.jsx"
 import { MailSvg } from "../header/mail-svg.jsx"
 
-const { Link } = ReactRouterDOM;
+const { Link } = ReactRouterDOM
 
 export function Cards() {
-    return (
-        <section className="cards-main-container">
-            <div className="cards-content-container main-layout">
-                <h1 className="cards-header-title">See What You Can Do</h1>
+  return (
+    <section className="cards-main-container">
+      <div className="cards-content-container main-layout">
+        <h1 className="cards-header-title">See What You Can Do</h1>
 
-                <div id="cards" className="cards-container">
+        <div id="cards" className="cards-container">
+          <div className="card">
+            <MailSvg />
+            <p>Stay up to date with our mail app.</p>
 
-                    <div className="card">
-                        <MailSvg />
-                        <p>My Mail</p>
-
-                        <Link to="/mail"><div className="learn-more-btn">Learn More</div></Link>
-                    </div>
-                    <div className="card">
-                        <KeepSvg />
-                        <p>My Notes</p>
-                        <Link to="/keep"><div className="learn-more-btn">Learn More</div></Link>
-                    </div>
-                    <div className="card">
-                        <BookSvg />
-                        <p>My Books</p>
-                        <Link to="/book"><div className="learn-more-btn">Learn More</div></Link>
-
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+            <Link to="/mail">
+              <div className="learn-more-btn">Try Me</div>
+            </Link>
+          </div>
+          <div className="card">
+            <KeepSvg />
+            <p>Tired of forgetting things?</p>
+            <p> With our note app it won't happened again!</p>
+            <Link to="/keep">
+              <div className="learn-more-btn">Try Me</div>
+            </Link>
+          </div>
+          <div className="card">
+            <BookSvg />
+            <p>You've got to broaden your horizons with our book library.</p>
+            <Link to="/book">
+              <div className="learn-more-btn">Try Me</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
